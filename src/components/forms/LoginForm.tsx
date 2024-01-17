9056a36d2653985b01b70886a34ba566a49209d6
Satya90jit <satyajitsahu2002@gmail.com>
2024-01-17 20:34:18 +0530

    Login SIgnUp Form Create

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { ArrowRight, ArrowRightAltOutlined } from "@mui/icons-material";
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <section>
+      <form action="#" method="POST" className="mt-8">
+        <div className="space-y-5">
+          <div>
+            <label htmlFor="" className="text-base font-medium text-gray-900">
+              {" "}
+              Email address{" "}
+            </label>
+            <div className="mt-2">
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="email"
+                placeholder="Email"
+              ></input>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="" className="text-base font-medium text-gray-900">
+                {" "}
+                Password{" "}
+              </label>
+              <a
+                href="#"
+                title=""
+                className="text-sm font-semibold text-black hover:underline"
+              >
+                {" "}
+                Forgot password?{" "}
+              </a>
+            </div>
+            <div className="mt-2">
+              <input
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="password"
+                placeholder="Password"
+              ></input>
+            </div>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="inline-flex w-full items-center justify-center rounded-md bg-green-600 hover:bg-black  hover:transition-all hover:origin-center   hover:duration-700 px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+            >
+              Login <ArrowRightAltOutlined className="ml-2" />
+            </button>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginForm;
