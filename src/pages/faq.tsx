129ea3c6c52260027d918a37d61d2efb3521eb65
Satya90jit <satyajitsahu2002@gmail.com>
2024-01-27 04:06:42 +0530

    Small Changes

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,9 +1,11 @@
+import { CommonHeroSection } from "@/components/core";
 import { PublicLayout } from "@/layouts";
 import React from "react";
 
 const FaqPage = () => {
   return (
     <PublicLayout title="FAQs | Edu-Hub-Learning">
+      <CommonHeroSection title="FAQs" />
       <section className="flex flex-col justify-center h-[60vh] ">
         <p className="text-primary text-7xl font-extrabold text-center">
           UNDER MAINTENANCE
--- src/pages/my-account/my-certificates.tsx
+++ src/pages/my-account/my-certificates.tsx
@@ -1,9 +1,11 @@
+import { CommonHeroSection } from "@/components/core";
 import { PublicLayout } from "@/layouts";
 import React from "react";
 
 const MyCertificates = () => {
   return (
     <PublicLayout title="My Certificates | Edu-Hub-Learning">
+      <CommonHeroSection title="My Certificates" />
       <section className="flex flex-row justify-center items-center h-screen">
         <div className="text-gray-900 text-5xl font-semibold">
           MY Certificates
